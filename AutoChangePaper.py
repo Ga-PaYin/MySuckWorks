d2857c7d25ac01fbbf80d2d5b8c50e681f8b267b
Ga-PaYin <36764890+Ga-PaYin@users.noreply.github.com>
2019-12-09 10:20:07 +0800

    Update AutoChangePaper.py
    新增默认壁纸，可以通过cmd使用

--- AutoChangePaper.py
+++ AutoChangePaper.py
@@ -2,26 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import time
+import random
+import sys
 import win32api
 import win32con
 import win32gui
 def getHTML(url):
-    try:
-        r = requests.get(url)
-        print("get html successfully")
-        r.raise_for_status()
-        r.encoding = r.apparent_encoding
-        #print(r.text)
-        return r.text
-    except:
-        return ""
+    for i in range(0,10):
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            r.encoding = r.apparent_encoding
+            print("get html successfully")
+            return r.text
+        except:
+            print("getHTML error")
+            if i < 9 :
+                print('tring again...')
+            else:
+                print('I tried')
+
 def parserHTML(html):
     try:
         soup = BeautifulSoup(html,"html.parser")
-        print(soup.find('a',attrs={'class':'preview'})['href'])
         return soup.find('a',attrs={'class':'preview'})['href']
     except:
-        pass
+        print('parseError')
 def getPhoto(html):
     try:
         soup = BeautifulSoup(html,"html.parser")
@@ -37,13 +43,14 @@ def savePicture(imag):
             os.mkdir(root)
         if not os.path.exists(path):
             print("downloading picture")
-            r = requests.get('http:' + imag)
+            print('imag = ' + imag)
+            r = requests.get(imag)
             print("downloaded")
             with open(path,'wb') as f:
                 f.write(r.content)
                 time.sleep(5)
                 f.close()
-                print("Picture Saved Successfully")
+                print("Picture Saved at " + path + " Successfully")
         else:
             print("Picture Exist")
         return path
@@ -53,19 +60,19 @@ def setWallPaper(Imag):
     # 打开指定注册表路径
     reg_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
     # 最后的参数:2拉伸,0居中,6适应,10填充,0平铺
-    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
+    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "10")
     # 最后的参数:1表示平铺,拉伸居中等都是0
     win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
     # 刷新桌面
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,Imag, win32con.SPIF_SENDWININICHANGE)
-def main():
-    keyword = input()
-    url = 'https://alpha.wallhaven.cc/search?q=' + keyword + '&categories=111&purity=110&sorting=random&order=desc'
+def main(keyword):
+    # keyword = input()
+    url = 'https://wallhaven.cc/search?q=' + keyword + '&categories=111&purity=100&atleast=2560x1920&sorting=random&order=desc'
     print("grasping photo from wallhaven...")
     HTMLpage = ''
     while HTMLpage == '':
         HTMLpage = getHTML(url)
-    print("grasp web successfully")
+    print("grap web successfully")
     photoAddress = parserHTML(HTMLpage)
     photo = getHTML(photoAddress)
     if photo != '':
@@ -74,4 +81,11 @@ def main():
         setWallPaper(LocalImag)
     
 if __name__ == '__main__':
-    main()
+    keyword = ''
+    if len(sys.argv) == 1:
+        keywordList = ['space','mountain','river','galaxy','moon','earth','sun','sky','planet','universe','rain','aurora']
+        keyword = keywordList[ random.randint(0,len(keywordList)) ]
+    else:
+        keyword = sys.argv[1]
+    print('keyword = ' + keyword)
+    main(keyword)
